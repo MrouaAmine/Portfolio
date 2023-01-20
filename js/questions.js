07bd6be31e6a27bf582e05f03dc0a3789bd169f9
MrouaAmine <116158893+MrouaAmine@users.noreply.github.com>
2023-01-20 14:17:33 +0100

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Que signifie HTML ?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Que signifie CSS ?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que signifie PHP ?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que signifie SQL ?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Que signifie XML ?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  }, 
+  {
+    numb: 6,
+    question: "Quelle année a vu la naissance de l'Internet ?",
+    answer: "1962",
+    options: [
+      "1962",
+      "1965",
+      "1989",
+      "1972"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Que signifie DNS ?",
+    answer: "Domain Name Server",
+    options: [
+      "Domain Name Server",
+      " Name Domain Server",
+      "Domain Server Name ",
+      "Domain Name"
+    ]
+  },
+    {
+    numb: 8,
+    question: "1 octet est égal à ?",
+    answer: "8 bits",
+    options: [
+      "8 bits",
+      "4 bits",
+      "3 bits",
+      "1 bits"
+    ]
+  },
+    {
+    numb: 9,
+    question: "3 octet est égal à ?",
+    answer: "24 bits",
+    options: [
+      "24 bits",
+      "12 bits",
+      "3 bits",
+      "1 bits"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Que signifie API ?",
+    answer: "Application Programming Interface",
+    options: [
+      "Application Programming Interface",
+      "Application Interface Programming ",
+      "Programming Application Interface",
+      "Interface Programming Application "
+    ]
+  },
+  
+];
